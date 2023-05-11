01516c4e39205729d046307ff9b3083218410562
blake-epistemix <blake.boucher@epistemix.com>
2023-05-11 14:38:13 -0500

    workflow test

---
+++ generate-manifest.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pip install tomlq
+
+touch manifest.toml && echo "" > manifest.toml
+
+for file in */fred.toml; do
+    filename=${file%/fred.toml}
+    metadata=$(tomlq -t '.metadata' "$file")
+    echo "[[metadata]]\n$metadata\nslug = \"$filename\"\n" >> manifest.toml
+done
+
+sed -i '' -e '1{/^$/d;}' manifest.toml
+sed -i '' -e '${/^$/d;}' manifest.toml
