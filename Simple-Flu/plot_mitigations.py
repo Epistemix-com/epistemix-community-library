442b88955cd34911e6503cf9aaea62fc5cb56c8e
Andrew Lane <andrew.lane@epistemix.com>
2023-10-18 16:05:39 +0000

    Add patch to simpleflu to include animation

--- Simple-Flu/plot_mitigations.py
+++ Simple-Flu/plot_mitigations.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import pandas as pd
 import numpy as np
 
@@ -33,3 +35,59 @@ def get_expmap_data(job):
     exposure_locations = exp_data[exp_data.my_exp_lat!=0].groupby(["my_exp_lat", "my_exp_lon", "ExposureLocation"]).size().to_frame().reset_index().rename(columns={0:"NumberExposed"})
     exposure_locations = exposure_locations.assign(exp_scale=np.log10(exposure_locations.NumberExposed+0.5))
     return exposure_locations
+
+
+def get_sim_exposures(df: pd.DataFrame) -> pd.DataFrame:
+    """Process `baseline_exposures` in preparation for plotting.
+    
+    To make an animated plot of exposures we need to make some tweaks to the
+    raw `baseline_exposures` DataFrame:
+    
+    1. Account for the fact that the model doesn't output correct
+       latitude/ longitude values for agents exposed outside of
+       Household, Workplace, or School. This is achieved by using
+       the agents' household location as a proxy for their exposure
+       location
+    2. Add 'dummy' exposures so that every date in the simulation
+       has the full set of exposure locations represented in its
+       legend. This is a workaround for the Plotly issue described
+       here https://github.com/plotly/plotly.js/issues/2861
+    """
+    return (
+        df
+        .pipe(_standardize_exposure_coords)
+        .pipe(_add_dummy_exposure_locations)
+    )
+
+
+def _standardize_exposure_coords(df: pd.DataFrame) -> pd.DataFrame:
+    """Handle fact that for InitialExposure, BlockGroup, CensusTract, County,
+    and Other, agent exposure locations are not well defined (fall back to
+    (0, 0)) so we use agents' household location where available instead.
+    """
+    def choose_plot_coord(row: pd.Series, coord_type: str) -> float:
+        default_col = f'my_exp_{coord_type}'
+        fallback_col = f'{coord_type}_house'
+        if row[default_col] != 0:
+            return row[default_col]
+        return row[fallback_col]
+
+    return (
+        df
+        .rename(columns={'long_house': 'lon_house'})
+        .assign(plot_lat=lambda df: df.apply(choose_plot_coord, coord_type='lat', axis=1))
+        .assign(plot_lon=lambda df: df.apply(choose_plot_coord, coord_type='lon', axis=1))
+    )
+
+
+def _add_dummy_exposure_locations(df: pd.DataFrame) -> pd.DataFrame:
+    """See https://github.com/plotly/plotly.js/issues/2861"""
+    return pd.concat([
+        df,
+        pd.DataFrame.from_records(
+        product(
+                pd.date_range(df['today'].min(), df['today'].max()),
+                df['ExposureLocation'].drop_duplicates()),
+            columns=['today', 'ExposureLocation']
+        )
+    ])
